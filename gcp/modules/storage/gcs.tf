488cc12d42e2220cfedaf9eeb1bbeacb16c15155
dehyohana <deh.yohana@gmail.com>
2024-06-20 21:26:48 -0300

    terraform apply

--- gcp/modules/storage/gcs.tf
+++ gcp/modules/storage/gcs.tf
@@ -1,7 +1,7 @@
 resource "google_storage_bucket" "terraform_bucket" {
   name                        = var.bucket_name
   location                    = var.location
-  storage_class               = "STANDART"
+  storage_class               = "STANDARD"
   uniform_bucket_level_access = true
   force_destroy               = false
 }
